a3225d83375f1f599167e977f67210e1f20b10f7
luis <luisfelipedemattos01@gmail.com.br>
2025-09-25 17:29:45 -0300

    estilo, navbar e img_site

--- paginas/index.php
+++ paginas/index.php
@@ -6,8 +6,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Shift Node</title>
     <link rel="icon" type="image/jpg" href="Shift Node"/>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        
+        <link rel="stylesheet" href="estilo/inicial.css">
 </head>
 <body>
-    
+
+<nav class="navbar">
+  <div class="container">
+
+        <div class="link">
+
+        <a href="index.php"><img class="logo" src="shiftnode.png" alt="shift_node" width="90" height="70"></a>
+
+        </div>
+
+        <div class="paginas">
+
+      <a class="link" href="agendamento.php">Agendar</a>
+     
+      <a href="serviços.php">Serviços</a>
+     
+      <a href="quem_somos.php">Quem Somos</a>
+
+      <a href="Contato.php">Contato</a>
+
+        </div>
+     
+    </a>
+  </div>
+</nav>
+
+    <img class="fundo_site" src="img_site/Fundo Site.jpg" alt="fundo">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
